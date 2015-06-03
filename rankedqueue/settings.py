f5b862ff7830327db2a026da37a73886b561715f
Ian Auld <imauld@gmail.com>
2015-06-03 15:20:29 -0700

    Changed settings to deploy

--- rankedqueue/settings.py
+++ rankedqueue/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '6opnmtmy0@t82j(^_l6xvo%0^+wjmzuex@hxe)=6qi!-%dka=x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
