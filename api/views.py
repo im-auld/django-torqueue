73307af0b40795fffd4f04851733b4452b07e81f
Ian Auld <imauld@gmail.com>
2015-08-16 10:47:43 -0700

    Added log in to view

--- api/views.py
+++ api/views.py
@@ -1,12 +1,25 @@
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render
-from models import Player, Server
 from rest_framework import viewsets, permissions
 from serializers import PlayerSerializer, ServerSerializer
+from api.forms import LoginForm
+from api.models import Player, Server
 
 
 def index(request):
+    login_form = LoginForm()
     queue = {server: [p for p in server] for server in Server.objects.all()}
-    context = {'queue': queue}
+    context = {
+        'queue': queue,
+        'login_form': login_form,
+    }
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user.is_active:
+            login(request, user)
+            context['user'] = user
     return render(request, 'api/index.html', context=context)
 
 class PlayerViewSet(viewsets.ModelViewSet):
