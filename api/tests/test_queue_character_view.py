75ec1cca1437e24016314b440535a2c172b4f3c2
Ian Auld <imauld@gmail.com>
2015-09-13 13:17:42 -0700

    Added test for queueing a character

--- api/tests/test_queue_character_view.py
+++ api/tests/test_queue_character_view.py
@@ -1,1 +1,24 @@
-__author__ = 'ian'
+from django.core.urlresolvers import reverse
+from django.test import TestCase
+from api.tests.mixins import QueueCharacterFixtureMixin
+
+
+class TestQueueCharacterView(QueueCharacterFixtureMixin, TestCase):
+    view_name = 'queue_character_view'
+
+    def test_anonymous_user(self):
+        url = reverse(self.view_name, kwargs={'character_id': self.character.pk})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(
+            response,
+            '{}{}{}'.format(reverse('index'), '?next=/queue/', self.character.pk)
+        )
+
+    def test_logged_in_user(self):
+        logged_in = self.client.login(username=self.user.username, password='password')
+        self.assertTrue(logged_in)
+        url = reverse(self.view_name, kwargs={'character_id': self.character.pk})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('index'))
